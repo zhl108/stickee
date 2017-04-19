a35c8b5a826c174188002fbe201a4a54cfc3fed3
zhi <zhl108@ucsd.edu>
2017-04-18 19:54:30 -0700

    Build Tool setup: gulp

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/hello', hello);
+app.use('/doc', express.static(path.join(__dirname, 'doc')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
