78a824c64528decbcadb41d87d591d72012d2e12
zhi <zhl108@ucsd.edu>
2017-04-19 18:47:12 -0700

    heroku auto deployment reset

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express: Heroku auto deployment pass' });
 });
 
 module.exports = router;
